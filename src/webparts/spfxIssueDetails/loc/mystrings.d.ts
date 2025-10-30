9ef6fa3ecc89f1066e8fbbf927fdf93f8a94d92a
Sanjeev Bastodkar <sanjeev.bastodkar@agr.gc.ca>
2025-10-30 11:15:41 -0400

    Initial check in

---
+++ src/webparts/spfxIssueDetails/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxIssueDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxIssueDetailsWebPartStrings' {
+  const strings: ISpfxIssueDetailsWebPartStrings;
+  export = strings;
+}
